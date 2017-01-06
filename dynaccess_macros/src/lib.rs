4772457647680bbd83efea4bd50ee4c5bdffb60f
Mitja Karhusaari <monttu300@gmail.com>
2017-01-06 21:51:15 +0200

    Add some documentation.

--- dynaccess_macros/src/lib.rs
+++ dynaccess_macros/src/lib.rs
@@ -1,3 +1,38 @@
+//! This crate provides `#[derive(Dynaccess)]` that implements dynamic accessor
+//! methods for the struct using `FieldAccessors` trait in `dynaccess_traits`.
+//! A module containing unit structs of which each represent one field in the
+//! struct. These structs are passed to the methods of `FieldAccessors` to get
+//! and modify the corresponding fields of the struct.
+//!
+//! # Example
+//!
+//! ```rust
+//! #[feature(proc_macro)]
+//! #[macro_use]
+//! extern create dynaccess_macros;
+//! extern crate dynaccess_traits;
+//!
+//! #[derive(Dynaccess)]
+//! pub struct Person {
+//!     pub age: u32,
+//!     pub names: Vec<String>,
+//! }
+//!
+//! fn main() {
+//!     let someone = Person {
+//!         age: 19,
+//!         names: vec!["John".to_string()],
+//!     };
+//!
+//!     assert_eq!(someone.get(field::Age), 19);
+//!     someone.set(field::Age, 20);
+//!     assert_eq!(someone.get(field::Age), 20);
+//!
+//!     someone.get_mut(field::Names).push("Smith");
+//!     assert_eq!(someone.get(field::Names).join(" "), "John Smith");
+//! }
+//! ```
+
 #![feature(proc_macro, proc_macro_lib)]
 #![recursion_limit = "1024"]
 
--- dynaccess_traits/src/lib.rs
+++ dynaccess_traits/src/lib.rs
@@ -1,17 +1,40 @@
+//! This crate contains traits used by `#[derive(Dynaccess)]`.
+
+/// Trait that is implemented for all unit structs representing
+/// struct fields.
 pub trait Field<S> {
+    /// The type of the field.
     type Type;
+
+    /// Visits a struct and sets the value of the field corresponding to
+    /// the implementor of this trait.
     fn set(struct_: &mut S, value: Self::Type);
+    
+    /// Visits a struct and returns a reference to the value of the field
+    /// corresponding to the implementor of this trait.
     fn get(struct_: &S) -> &Self::Type;
+
+    /// Visits a struct and returns a mutable reference to the value of
+    /// the field corresponding to the implementor of this trait.
     fn get_mut(struct_: &mut S) -> &mut Self::Type;
 }
 
+/// Trait that is implemented for the struct `#[derive(Dynaccess)]` is used on.
 pub trait FieldAccessors {
+    /// Calls `F::set(self, value)` to set the value of the field the passed
+    /// unit struct represents.
     fn set<F,V>(&mut self, field: F, value: V)
         where F: Field<Self, Type=V>,
               Self: Sized;
+
+    /// Calls `F::get(self, value)` to get a reference to the value of the field
+    /// the passed unit struct represents.
     fn get<F,V>(& self, field: F) -> &V
         where F: Field<Self, Type=V>,
               Self: Sized;
+    
+    /// Calls `F::get_mut(self, value)` to get a mutable reference to the value
+    /// of the field the passed unit struct represents.
     fn get_mut<F,V>(&mut self, field: F) -> &mut V
         where F: Field<Self, Type=V>,
               Self: Sized;
